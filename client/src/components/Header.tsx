65ce4adef0dbef46e35d334057c57372882d90ab
PaulPintang <paulpintang.dev@gmail.com>
2023-06-17 12:30:04 +0800

    UI: admin select course

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -9,13 +9,19 @@ import {
   Text,
   Menu,
   Badge,
+  Stack,
 } from "@mantine/core";
 import { NavLink, useLocation, useNavigate } from "react-router-dom";
 import { useAppDispatch, useAppSelector } from "../app/hooks";
 import logo from "../assets/Pixel8-Official-Logo.jpg";
 import avatar from "../assets/avatar.png";
 import { useGoogleLogin } from "@react-oauth/google";
-import { IconLogout } from "@tabler/icons-react";
+import {
+  IconArrowRightTail,
+  IconCheck,
+  IconLogout,
+  IconSelector,
+} from "@tabler/icons-react";
 import axios from "axios";
 import { setUser, logout } from "../features/auth/authSlice";
 import {
@@ -73,7 +79,7 @@ const Header = () => {
     <Flex justify="space-between" align="center">
       <Image src={logo} width={205} className="relative" left={-20} />
       <Group>
-        {user && pathname !== "/" && (
+        {user && user.role !== "admin" && pathname !== "/" && (
           <NavLink to="/" className="text-white">
             <Button size="xs" color="cyan" variant="white">
               Documentation
@@ -96,9 +102,91 @@ const Header = () => {
         )}
 
         {user?.role === "admin" && (
-          <Badge color="teal" variant="dot" className="text-gray-700">
-            Administrator
-          </Badge>
+          <>
+            <Menu shadow="md" position="bottom">
+              <Menu.Target>
+                <Button
+                  rightIcon={<IconSelector size={15} />}
+                  variant="white"
+                  size="xs"
+                  color="dark"
+                >
+                  Software Development
+                </Button>
+              </Menu.Target>
+
+              <Menu.Dropdown className="pr-3">
+                <Stack spacing={3} w={220} p={2}>
+                  <Flex
+                    justify="space-between"
+                    align="center"
+                    className="cursor-pointer"
+                  >
+                    <Button
+                      leftIcon={
+                        <Badge
+                          color="teal"
+                          variant="filled"
+                          radius="sm"
+                          size="xs"
+                          w={21}
+                        >
+                          S
+                        </Badge>
+                      }
+                      variant="white"
+                      size="xs"
+                      color="dark"
+                    >
+                      Software Analyst
+                    </Button>
+                    {/* <IconCheck size={17} /> */}
+                  </Flex>
+                  <Flex justify="space-between" align="center">
+                    <Button
+                      leftIcon={
+                        <Badge
+                          color="grape"
+                          variant="filled"
+                          radius="sm"
+                          size="xs"
+                          w={21}
+                        >
+                          S
+                        </Badge>
+                      }
+                      variant="white"
+                      size="xs"
+                      color="dark"
+                    >
+                      Software Development
+                    </Button>
+                    <IconCheck size={17} />
+                  </Flex>
+                  <Flex justify="space-between" align="center">
+                    <Button
+                      leftIcon={
+                        <Badge
+                          color="orange"
+                          variant="filled"
+                          radius="sm"
+                          size="xs"
+                          w={21}
+                        >
+                          U
+                        </Badge>
+                      }
+                      variant="white"
+                      size="xs"
+                      color="dark"
+                    >
+                      UI/UX Designer
+                    </Button>
+                  </Flex>
+                </Stack>
+              </Menu.Dropdown>
+            </Menu>
+          </>
         )}
 
         {user ? (
