b7e3ab22af71f1f7e291bff1a315ed0a5ecc9196
PaulPintang <paulpintang.dev@gmail.com>
2023-06-28 17:58:08 +0800

    fix: on dtr time out, update also the task spent.

--- client/src/routes/Dashboard/components/TasksTodoCard.tsx
+++ client/src/routes/Dashboard/components/TasksTodoCard.tsx
@@ -63,67 +63,83 @@ const TasksTodoCard = () => {
   return (
     <>
       <Card className="h-full rounded-md shadow-md ">
-        {currentTask?.todos?.length !== 0 ? (
+        {currentTask ? (
           <>
-            <Group spacing={10} pb={5}>
-              <Text fz="sm" className="text-gray-500 font-semibold">
-                Current Task:
-              </Text>
-              <Text fz="sm" c="dimmed">
-                {currentTask?.taskname}
-              </Text>
-            </Group>
-            <Flex align="center" justify="space-between" pb={3}>
-              <Text fz="xs" className="text-gray-500 font-semibold">
-                You've got {currentTask?.todos?.length} todos for this task
-              </Text>
-              <Button size="xs" variant="white" onClick={toggle}>
-                + Add more
-              </Button>
-            </Flex>
-            <ScrollArea.Autosize mah={220} scrollbarSize={8}>
-              <div className="space-y-3 ">
-                {currentTask?.todos?.map((todo, index) => (
-                  <div className="bg-slate-50 opacity-70 px-3 py-2 rounded-md relative">
-                    <Text c="dark" fz="xs" className="w-[80%]" py={4}>
-                      <span
-                        className={todo.isDone ? "line-through italic" : ""}
-                      >
-                        {todo.todo}
-                      </span>
-                    </Text>
-                    <div className="absolute top-2 right-3">
-                      <Group spacing={6}>
-                        <Checkbox
-                          checked={todo.isDone}
-                          size="xs"
-                          color="cyan"
-                          onChange={() => isTodoDone(index)}
-                        />
-                        <ActionIcon
-                          color="red"
-                          variant="light"
-                          radius="xl"
-                          onClick={() => removeTodo(index)}
-                        >
-                          <IconX size={15} />
-                        </ActionIcon>
-                      </Group>
-                    </div>
+            {currentTask?.todos?.length !== 0 ? (
+              <>
+                <Group spacing={10} pb={5}>
+                  <Text fz="sm" className="text-gray-500 font-semibold">
+                    Current Task:
+                  </Text>
+                  <Text fz="sm" c="dimmed">
+                    {currentTask?.taskname}
+                  </Text>
+                </Group>
+                <Flex align="center" justify="space-between" pb={3}>
+                  <Text fz="xs" className="text-gray-500 font-semibold">
+                    You've got {currentTask?.todos?.length} todos for this task
+                  </Text>
+                  <Button size="xs" variant="white" onClick={toggle}>
+                    + Add more
+                  </Button>
+                </Flex>
+                <ScrollArea.Autosize mah={220} scrollbarSize={8}>
+                  <div className="space-y-3 ">
+                    {currentTask?.todos?.map((todo, index) => (
+                      <div className="bg-slate-50 opacity-70 px-3 py-2 rounded-md relative">
+                        <Text c="dark" fz="xs" className="w-[80%]" py={4}>
+                          <span
+                            className={todo.isDone ? "line-through italic" : ""}
+                          >
+                            {todo.todo}
+                          </span>
+                        </Text>
+                        <div className="absolute top-2 right-3">
+                          <Group spacing={6}>
+                            <Checkbox
+                              checked={todo.isDone}
+                              size="xs"
+                              color="cyan"
+                              onChange={() => isTodoDone(index)}
+                            />
+                            <ActionIcon
+                              color="red"
+                              variant="light"
+                              radius="xl"
+                              onClick={() => removeTodo(index)}
+                            >
+                              <IconX size={15} />
+                            </ActionIcon>
+                          </Group>
+                        </div>
+                      </div>
+                    ))}
                   </div>
-                ))}
-              </div>
-            </ScrollArea.Autosize>
+                </ScrollArea.Autosize>
+              </>
+            ) : (
+              <Flex direction="column" align="center" gap={10}>
+                <Image src={empty} width={130} />
+                <Text fw="bold" c="dark">
+                  Don't have any new todo?
+                </Text>
+                <Button size="sm" color="cyan" onClick={toggle}>
+                  Add todos
+                </Button>
+              </Flex>
+            )}
           </>
         ) : (
           <Flex direction="column" align="center" gap={10}>
             <Image src={empty} width={130} />
-            <Text fw="bold" c="dark">
-              Don't have any new todo?
-            </Text>
-            <Button size="sm" color="cyan" onClick={toggle}>
-              Add todos
-            </Button>
+            <div className="text-center">
+              <Text fw="bold" c="dark">
+                You don't have inprogress task
+              </Text>
+              <Text fz="xs" c="dimmed">
+                You can only add todos if you have inprogress task
+              </Text>
+            </div>
           </Flex>
         )}
       </Card>
