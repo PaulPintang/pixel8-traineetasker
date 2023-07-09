2c38adb75d95068679d6409e2249263ae4b180be
PaulPintang <paulpintang.dev@gmail.com>
2023-07-09 14:10:51 +0800

    env

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,7 +7,6 @@ import cookieParser from "cookie-parser";
 import http from "http";
 import { TokenPayload } from "./interfaces/user.interface";
 import { Server } from "socket.io";
-import { ITask } from "./interfaces/task.interface";
 
 dotenv.config();
 
@@ -27,13 +26,13 @@ app.use(express.json({ limit: "200mb" }));
 app.use(
   cors({
     credentials: true,
-    origin: "http://localhost:5173",
+    origin: "https://traineetasker.vercel.app/",
   })
 );
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://traineetasker.vercel.app/",
     credentials: true,
   },
 });
