2981aa47649bd0b974f697e751ec4e3fbca78c38
PaulPintang <paulpintang.dev@gmail.com>
2023-07-19 23:30:51 +0800

    test: refactor timespent function

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -65,70 +65,69 @@ const Header = () => {
       });
   };
 
-  function addTimeStrings(time1, time2) {
-    const timeToMinutes = (time) => {
-      const regex = /^((\d+)day(s)?)? ?((\d+)hr(s)?)? ?((\d+)min(s)?)?$/;
-      const match = time.match(regex);
+  // function addTimeStrings(time1, time2) {
+  //   const timeToMinutes = (time) => {
+  //     const regex = /^((\d+)day(s)?)? ?((\d+)hr(s)?)? ?((\d+)min(s)?)?$/;
+  //     const match = time.match(regex);
 
-      if (match) {
-        const days = match[2] ? parseInt(match[2]) : 0;
-        const hrs = match[5] ? parseInt(match[5]) : 0;
-        const mins = match[8] ? parseInt(match[8]) : 0;
-        return days * 24 * 60 + hrs * 60 + mins;
-      } else {
-        throw new Error(
-          `Invalid time format: ${time}. Expected format: "XXdays XXhrs XXmins".`
-        );
-      }
-    };
+  //     if (match) {
+  //       const days = match[2] ? parseInt(match[2]) : 0;
+  //       const hrs = match[5] ? parseInt(match[5]) : 0;
+  //       const mins = match[8] ? parseInt(match[8]) : 0;
+  //       return days * 24 * 60 + hrs * 60 + mins;
+  //     } else {
+  //       throw new Error(
+  //         `Invalid time format: ${time}. Expected format: "XXdays XXhrs XXmins".`
+  //       );
+  //     }
+  //   };
 
-    const minutesToTime = (minutes) => {
-      const days = Math.floor(minutes / (24 * 60));
-      minutes %= 24 * 60;
-      const hrs = Math.floor(minutes / 60);
-      const mins = minutes % 60;
+  //   const minutesToTime = (minutes) => {
+  //     const days = Math.floor(minutes / (24 * 60));
+  //     minutes %= 24 * 60;
+  //     const hrs = Math.floor(minutes / 60);
+  //     const mins = minutes % 60;
 
-      let timeString = "";
-      if (days > 0) {
-        timeString += `${days}day${days > 1 ? "s" : ""}`;
-      }
-      if (hrs > 0) {
-        if (timeString) {
-          timeString += " ";
-        }
-        timeString += `${hrs}hr${hrs > 1 ? "s" : ""}`;
-      }
-      if (mins > 0) {
-        if (timeString) {
-          timeString += " ";
-        }
-        timeString += `${mins}min${mins > 1 ? "s" : ""}`;
-      }
+  //     let timeString = "";
+  //     if (days > 0) {
+  //       timeString += `${days}day${days > 1 ? "s" : ""}`;
+  //     }
+  //     if (hrs > 0) {
+  //       if (timeString) {
+  //         timeString += " ";
+  //       }
+  //       timeString += `${hrs}hr${hrs > 1 ? "s" : ""}`;
+  //     }
+  //     if (mins > 0) {
+  //       if (timeString) {
+  //         timeString += " ";
+  //       }
+  //       timeString += `${mins}min${mins > 1 ? "s" : ""}`;
+  //     }
 
-      return timeString;
-    };
+  //     return timeString;
+  //   };
 
-    try {
-      const minutes1 = timeToMinutes(time1);
-      const minutes2 = timeToMinutes(time2);
-      const totalMinutes = minutes1 + minutes2;
+  //   try {
+  //     const minutes1 = timeToMinutes(time1);
+  //     const minutes2 = timeToMinutes(time2);
+  //     const totalMinutes = minutes1 + minutes2;
 
-      console.log(minutesToTime(totalMinutes));
-    } catch (error) {
-      console.error(error.message);
-      return ""; // Return an empty string or appropriate value if there's an error.
-    }
-  }
+  //     console.log(minutesToTime(totalMinutes));
+  //   } catch (error) {
+  //     console.error(error.message);
+  //     return ""; // Return an empty string or appropriate value if there's an error.
+  //   }
+  // }
 
   // Example usage:
-  const time1 = "20mins";
-  const time2 = "1day20mins";
+  // const time1 = "20mins";
+  // const time2 = "1day20mins";
   // const totalTime = addTimeStrings(time1, time2);
   // console.log(totalTime);
 
   return (
     <Flex justify="space-between" align="center">
-      <button onClick={() => addTimeStrings(time1, time2)}>test</button>
       <NavLink to="/" className="text-white">
         <div className="w-[160px] md:w-[180px] lg:w-[180px]">
           <Image src={logo} className="relative" left={-20} />
--- server/src/utils/calculateSpentTime.ts
+++ server/src/utils/calculateSpentTime.ts
@@ -37,9 +37,6 @@ export const calculateSpentTime = (time: TimeSpent) => {
     }`
   );
 
-  // const morningTimeDiff = Math.abs(
-  //   time.morning?.end !== "" ? morningEnd.getTime() - morningStart.getTime() :
-  // );
   const morningTimeDiff = Math.abs(
     morningEnd.getTime() - morningStart.getTime()
   );
@@ -103,3 +100,58 @@ export const calculateSpentTime = (time: TimeSpent) => {
 
   return spent;
 };
+
+export function addTimeStrings(time1: string, time2: string) {
+  const timeToMinutes = (time: string) => {
+    const regex = /^((\d+)day(s)?)? ?((\d+)hr(s)?)? ?((\d+)min(s)?)?$/;
+    const match = time.match(regex);
+
+    if (match) {
+      const days = match[2] ? parseInt(match[2]) : 0;
+      const hrs = match[5] ? parseInt(match[5]) : 0;
+      const mins = match[8] ? parseInt(match[8]) : 0;
+      return days * 24 * 60 + hrs * 60 + mins;
+    } else {
+      throw new Error(
+        `Invalid time format: ${time}. Expected format: "XXdays XXhrs XXmins".`
+      );
+    }
+  };
+
+  const minutesToTime = (minutes: number) => {
+    const days = Math.floor(minutes / (24 * 60));
+    minutes %= 24 * 60;
+    const hrs = Math.floor(minutes / 60);
+    const mins = minutes % 60;
+
+    let timeString = "";
+    if (days > 0) {
+      timeString += `${days}day${days > 1 ? "s" : ""}`;
+    }
+    if (hrs > 0) {
+      if (timeString) {
+        timeString += " ";
+      }
+      timeString += `${hrs}hr${hrs > 1 ? "s" : ""}`;
+    }
+    if (mins > 0) {
+      if (timeString) {
+        timeString += " ";
+      }
+      timeString += `${mins}min${mins > 1 ? "s" : ""}`;
+    }
+
+    return timeString;
+  };
+
+  try {
+    const minutes1 = timeToMinutes(time1);
+    const minutes2 = timeToMinutes(time2);
+    const totalMinutes = minutes1 + minutes2;
+
+    return minutesToTime(totalMinutes);
+  } catch (error) {
+    console.error(error.message);
+    return ""; // Return an empty string or appropriate value if there's an error.
+  }
+}
