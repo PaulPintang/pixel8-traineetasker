0ddb09bea3e3b9276b96951ecbb969e5e02a33d6
PaulPintang <paulpintang.dev@gmail.com>
2023-07-22 23:16:55 +0800

    trainee controller time string

--- server/src/controllers/traineeController.ts
+++ server/src/controllers/traineeController.ts
@@ -125,11 +125,11 @@ export const timeInOutDTR = asyncHandler(
             );
           }
         } else if (trainee.dtr[todayIndex].afternoon.in === "") {
-          trainee.dtr[todayIndex].afternoon.in = "01:00 PM";
-          // trainee.dtr[todayIndex].afternoon.in = format.time;
+          // trainee.dtr[todayIndex].afternoon.in = "01:00 PM";
+          trainee.dtr[todayIndex].afternoon.in = format.time;
           if (recording !== -1) {
-            trainee.timesheet[recording].afternoon.start = "01:00 PM";
-            // trainee.timesheet[recording].afternoon.start = format.time;
+            // trainee.timesheet[recording].afternoon.start = "01:00 PM";
+            trainee.timesheet[recording].afternoon.start = format.time;
           }
         } else if (trainee.dtr[todayIndex].afternoon.out === "") {
           const hours = handleTraineeHourSpent(trainee, "afternoon");
@@ -183,8 +183,8 @@ export const timeInOutDTR = asyncHandler(
           date: format.date,
           status: "recording",
           morning: {
-            in: "08:00 AM",
-            // in: format.time,
+            // in: "08:00 AM",
+            in: format.time,
             out: "",
           },
           afternoon: {
@@ -200,8 +200,8 @@ export const timeInOutDTR = asyncHandler(
             status: "recording",
             date: format.date,
             morning: {
-              // start: format.time,
-              start: "8:00 AM",
+              start: format.time,
+              // start: "8:00 AM",
               end: "",
             },
             afternoon: {
